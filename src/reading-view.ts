ca6523d5b4f54a7ceda4122a6d85969c6c4de797
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2023-11-29 05:40:23 +0900

    add li support in reading view

--- src/reading-view.ts
+++ src/reading-view.ts
@@ -2,7 +2,7 @@ import { MarkdownPostProcessorContext } from "obsidian";
 import { isCJK } from "utils";
 
 export const dynamicLineHeightPostProcessor = (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
-    for (const paragraph of el.querySelectorAll('p')) {
+    for (const paragraph of el.querySelectorAll('p, li')) {
         if (paragraph.textContent) {
             if (Array.from(paragraph.textContent).some(char => isCJK(char))) {
                 paragraph.classList.add('cjk');
